fde7bd567ad2e6c6845751235356a195ff4494fa
DmitryLenko <dias1381254@gmail.com>
2018-10-28 11:46:28 +0300

    feat: add tests for make function and add function make implementation

--- src/make.js
+++ src/make.js
@@ -1,3 +1,12 @@
-function make() {
-
+module.exports = function make(...value) {
+    let arr = value;
+    let carring = (...value) => {
+      if(typeof value[0] === "function") {
+          arr = arr.reduce((acc, item) => value[0](acc, item))
+        return arr;
+      }
+      arr.push(...value);
+      return carring;
+    };
+    return carring;
 }
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const sumOfOther = require('./src/sum-of-other');
+const make = require('./src/make');
 
 describe('sumofother', () => {
   it('arr1', () => {
@@ -20,5 +21,24 @@ describe('sumofother', () => {
   it('arr4', () => {
     const arr4 = sumOfOther([5, 2, 1, 7, 8]);
     assert.deepEqual(arr4, [18, 21 , 22, 16, 15]);
+  });
+});
+
+describe('make', () => {
+  it('test1', () => {
+    function sum(a, b) {
+      return a + b;
+    }
+    const test1 = make(15)(34, 21, 666)(41)(sum);
+    assert.deepEqual(test1, 777);
+  });
+
+  it('test2', () => {
+    function divide(a,b) {
+      return a-b;
+    }
+
+    const test2 = make(15)(41)(divide);
+    assert.deepEqual(test2, -26);
   });
 });
